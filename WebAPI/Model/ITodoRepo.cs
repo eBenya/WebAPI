382fbb94457fa3faea136e7e7f717812a71735e9
eBenya <MyMail@com>
2020-11-11 10:59:24 +0300

    create interface CRUD operations

---
+++ WebAPI/Model/ITodoRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Model
+{
+    interface ITodoRepo
+    {
+        void Add(TodoItem item);
+        IEnumerable<TodoItem> GetAll();
+        TodoItem Find(Guid key);
+        TodoItem Remove(Guid key);
+        void Update(TodoItem item);
+    }
+}
