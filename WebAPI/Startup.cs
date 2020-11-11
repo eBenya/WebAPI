a787a30fc6f5510f3b21b2ff138bd8f8b4b42065
eBenya <MyMail@com>
2020-11-11 11:03:14 +0300

    register TodoRepo on IoC

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WebAPI.Model;
 
 namespace WebAPI
 {
@@ -25,7 +26,9 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddMvc();
+            services.AddSingleton<ITodoRepo, TodoRepo>();
+            //services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
