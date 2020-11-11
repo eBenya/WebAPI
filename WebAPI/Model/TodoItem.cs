3de6e86b1d60bba90448c49595347b3b4de374ec
eBenya <MyMail@com>
2020-11-11 10:58:27 +0300

    create model

---
+++ WebAPI/Model/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Model
+{
+    public class TodoItem
+    {
+        public Guid Key { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
